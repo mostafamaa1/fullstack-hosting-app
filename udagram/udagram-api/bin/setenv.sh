c0efcc16607bf221e73204ca3fee70d150b1ae3a
Mostafa <mostafaahmedmaa1@gmail.com>
2022-11-04 18:24:58 +0200

    updated

--- udagram/udagram-api/bin/setenv.sh
+++ udagram/udagram-api/bin/setenv.sh
@@ -1,4 +1,2 @@
 # set env vars to elastic beanstalk
-eb init udagram-api --region us-east-1 --platform node.js
-eb use udagram-api-dev
 eb setenv AWS_BUCKET=${AWS_BUCKET} JWT_SECRET=${JWT_SECRET} PORT=${PORT} POSTGRES_DB=${POSTGRES_DB} POSTGRES_HOST=${POSTGRES_HOST} POSTGRES_PASSWORD=${POSTGRES_PASSWORD} POSTGRES_PORT=${POSTGRES_PORT} POSTGRES_USERNAME=${POSTGRES_USERNAME}
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-chkjrke3.us-east-1.elasticbeanstalk.com/api/v0/",
+  production: false,
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.eba-chkjrke3.us-east-1.elasticbeanstalk.com/api/v0/',
 };
 
 /*
