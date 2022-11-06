c6e7f40c0725b753e34682e7ee359c0e86b18ae3
Mostafa <mostafaahmedmaa1@gmail.com>
2022-11-06 14:22:45 +0200

    update commit

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -3,4 +3,7 @@ eb init udagram-api --region us-east-1 --platform node.js
 eb create --sample udagram-api-dev
 eb use udagram-api-dev
 
+eb setenv AWS_BUCKET=$AWS_BUCKET AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION POSTGRES_PORT=$POSTGRES_PORT PORT=$PORT JWT_SECRET=$JWT_SECRET
+POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_USERNAME=$POSTGRES_USERNAME URL=$URL
+
 eb deploy udagram-api-dev
